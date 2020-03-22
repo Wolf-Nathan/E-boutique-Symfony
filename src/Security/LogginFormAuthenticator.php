677d1dadfcfaf967a5107854dc896419f544f5d4
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-22 18:06:31 +0100

    Config security.yaml

--- src/Security/LogginFormAuthenticator.php
+++ src/Security/LogginFormAuthenticator.php
@@ -95,7 +95,9 @@ class LogginFormAuthenticator extends AbstractFormLoginAuthenticator implements 
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('index'));
+
     }
 
     protected function getLoginUrl()

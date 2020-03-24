1a245d0fd60380ff28155cb2b230ff9e234da81a
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-24 12:09:48 +0100

    Create new twig extension to format phone number

--- src/Twig/CustomsExtensions.php
+++ src/Twig/CustomsExtensions.php
@@ -14,12 +14,21 @@ class CustomsExtensions extends AbstractExtension {
     public function getFunctions()
     {
         return [
-          new TwigFunction('getLength', [$this, 'getLength'])
+          new TwigFunction('getLength', [$this, 'getLength']),
+          new TwigFunction('phoneNumber', [$this, 'phoneNumber'])
         ];
     }
 
     public function getLength(PersistentCollection $arrayCollection){
         $array = $arrayCollection->toArray();
         return count($array);
+    }
+
+    public function phoneNumber(string $phoneNumber){
+        return $phoneNumber[0] . $phoneNumber[1] . ' ' .
+               $phoneNumber[2] . $phoneNumber[3] . ' ' .
+               $phoneNumber[4] . $phoneNumber[5] . ' ' .
+               $phoneNumber[6] . $phoneNumber[7] . ' ' .
+               $phoneNumber[8] . $phoneNumber[9];
     }
 }

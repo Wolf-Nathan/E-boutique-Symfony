5d2ba7db577a416a134c804cb28bf08bcc6676ba
Wolf Nathan <nathan-w@hotmail.fr>
2020-03-24 17:01:49 +0100

    Start to make edit profile template

---
+++ src/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+class UserController extends AbstractController{
+
+    /**
+     * @Route("/{id}/edit", name="user_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, User $user): Response
+    {
+        //$form = $this->createForm(UserType::class, $user);
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('nom', TextType::class)
+            ->add('prenom', TextType::class)
+            ->add('telephone', TextType::class)
+            ->add('password', PasswordType::class)
+            ->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('profil');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+}
